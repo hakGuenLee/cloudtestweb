cf1b79227a2ebb640084501f88768fb39eabdc5e
hakGuenLee <alsfkr23@naver.com>
2024-08-09 07:30:04 +0900

    업로드

---
+++ cloudtestweb/src/test/java/home/myblog/blog/CloudtestwebApplicationTests.java
@@ -0,0 +1,13 @@
+package home.myblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudtestwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
